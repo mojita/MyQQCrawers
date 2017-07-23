98fa6295d28333e73495a2212679a3c2ec501525
mojita <rsystem392@gmail.com>
2017-07-23 11:06:33 +0800

    Initial commit

---
+++ src/main/java/com/mojita/cn/MyQqCrawersApplication.java
@@ -0,0 +1,12 @@
+package com.mojita.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQqCrawersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyQqCrawersApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mojita/cn/MyQqCrawersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mojita.cn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyQqCrawersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
